308be6f6102a3aad31c3a70e74028d94bac10583
Sagi <sagifogel@hotmail.com>
2016-04-20 11:48:33 +0300

    added ambient module declaration

--- type_definitions/TSFunq.d.ts
+++ type_definitions/TSFunq.d.ts
@@ -1,91 +1,95 @@
-﻿declare enum Owner {
-    container = 0,
-    external = 1
+﻿declare namespace TSFunq {
+    export enum Owner {
+        container = 0,
+        external = 1
+    }
+
+    export enum ReuseScope {
+        container = 0,
+        hierarchy = 1,
+        none = 2
+    }
+
+    export interface IDisposable {
+        dispose(): void;
+    }
+
+    export interface IOwned {
+        ownedBy(owner: Owner): void;
+    }
+
+    export interface IReused {
+        reusedWithin(scope: ReuseScope): IOwned;
+    }
+
+    export interface IReusedOwned extends IReused, IOwned {
+    }
+
+    export interface IRegistration extends IReusedOwned {
+    }
+
+    export interface IGenericRegistration<TService> extends IRegistration, IInitializable<TService> {
+    }
+
+    export interface IInitializable<TService> {
+        initializedBy(initializer: Action<IRegistrationResolver, TService>): IReusedOwned;
+    }
+
+    interface IRegistry {
+        registerInstance<TService>(instance: TService): void;
+        registerNamedInstance<TService>(name: string, instance: TService): void;
+        register<TService>(ctor: Constructor<TService>, factory: (container: IContainer) => TService): IGenericRegistration<TService>;
+        registerNamed<TService>(ctor: Constructor<TService>, name: string, factory: (container: IContainer) => TService): IGenericRegistration<TService>;
+        register<TService, TArg1>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1) => TService): IGenericRegistration<TService>;
+        registerNamed<TService, TArg1>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1) => TService): IGenericRegistration<TService>;
+        register<TService, TArg1, TArg2>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1, arg2: TArg2) => TService): IGenericRegistration<TService>;
+        register<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3) => TService): IGenericRegistration<TService>;
+        registerNamed<TService, TArg1, TArg2>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1, arg2: TArg2) => TService): IGenericRegistration<TService>;
+        registerNamed<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3) => TService): IGenericRegistration<TService>;
+        register<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4) => TService): IGenericRegistration<TService>;
+        registerNamed<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4) => TService): IGenericRegistration<TService>;
+    }
+
+    export interface IRegistrationResolver {
+        resolve<TService>(ctor: Constructor<TService>): TService;
+        tryResolve<TService>(ctor: Constructor<TService>): TService;
+        lazyResolve<TService>(ctor: Constructor<TService>): () => TService;
+        resolveNamed<TService>(ctor: Constructor<TService>, name: string): TService;
+        resolve<TService, TArg1>(ctor: Constructor<TService>, arg1: TArg1): TService;
+        tryResolveNamed<TService>(ctor: Constructor<TService>, name: string): TService;
+        tryResolve<TService, TArg1>(ctor: Constructor<TService>, arg1: TArg1): TService;
+        lazyResolve<TService, TArg1>(ctor: Constructor<TService>, arg1: TArg1): TService;
+        lazyResolveNamed<TService>(ctor: Constructor<TService>, name: string): () => TService;
+        resolveNamed<TService, TArg1>(ctor: Constructor<TService>, name: string, arg1: TArg1): TService;
+        resolve<TService, TArg1, TArg2>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2): TService;
+        tryResolve<TService, TArg1, TArg2>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2): TService;
+        lazyResolveNamed<TService, TArg1>(ctor: Constructor<TService>, name: string, arg1: TArg1): TService;
+        lazyResolve<TService, TArg1, TArg2>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2): TService;
+        resolveNamed<TService, TArg1, TArg2>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2): TService;
+        resolve<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
+        tryResolve<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, arg1: TArg1, arg: TArg2, arg3: TArg3): TService;
+        lazyResolveNamed<TService, TArg1, TArg2>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2): TService;
+        lazyResolve<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
+        resolveNamed<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
+        resolve<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
+        lazyResolveNamed<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
+        tryResolve<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
+        lazyResolve<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
+        resolveNamed<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
+        lazyResolveNamed<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
+    }
+
+    export interface IContainer extends IRegistrationResolver, IRegistry, IDisposable {
+    }
+
+    export interface IContainerConstructor {
+        new (): IContainer;
+    }
+
+    export var Container: IContainerConstructor;
+    export type Constructor<TService> = (new (...args: any[]) => TService) | string;
 }
 
-declare enum ReuseScope {
-    container = 0,
-    hierarchy = 1,
-    none = 2
+declare module "TSFunq" {
+    export = TSFunq;
 }
-
-interface IHashable {
-    getHashCode(): number;
-}
-
-interface IDisposable {
-    dispose(): void;
-}
-
-
-interface IOwned {
-    ownedBy(owner: Owner): void;
-}
-
-interface IReused {
-    reusedWithin(scope: ReuseScope): IOwned;
-}
-
-interface IReusedOwned extends IReused, IOwned {
-}
-
-interface IRegistration extends IReusedOwned {
-}
-
-interface IGenericRegistration<TService> extends IRegistration, IInitializable<TService> {
-}
-
-interface IInitializable<TService> {
-    initializedBy(initializer: Action<IRegistrationResolver, TService>): IReusedOwned;
-}
-
-interface IRegistry {
-    registerInstance<TService>(instance: TService): void;
-    registerNamedInstance<TService>(name: string, instance: TService): void;
-    register<TService>(ctor: Constructor<TService>, factory: (container: IContainer) => TService): IGenericRegistration<TService>;
-    registerNamed<TService>(ctor: Constructor<TService>, name: string, factory: (container: IContainer) => TService): IGenericRegistration<TService>;
-    register<TService, TArg1>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1) => TService): IGenericRegistration<TService>;
-    registerNamed<TService, TArg1>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1) => TService): IGenericRegistration<TService>;
-    register<TService, TArg1, TArg2>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1, arg2: TArg2) => TService): IGenericRegistration<TService>;
-    register<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3) => TService): IGenericRegistration<TService>;
-    registerNamed<TService, TArg1, TArg2>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1, arg2: TArg2) => TService): IGenericRegistration<TService>;
-    registerNamed<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3) => TService): IGenericRegistration<TService>;
-    register<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4) => TService): IGenericRegistration<TService>;
-    registerNamed<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, name: string, factory: (container: IContainer, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4) => TService): IGenericRegistration<TService>;
-}
-
-interface IContainer extends IRegistrationResolver, IRegistry, IDisposable {
-}
-
-interface IRegistrationResolver {
-    resolve<TService>(ctor: Constructor<TService>): TService;
-    tryResolve<TService>(ctor: Constructor<TService>): TService;
-    lazyResolve<TService>(ctor: Constructor<TService>): () => TService;
-    resolveNamed<TService>(ctor: Constructor<TService>, name: string): TService;
-    resolve<TService, TArg1>(ctor: Constructor<TService>, arg1: TArg1): TService;
-    tryResolveNamed<TService>(ctor: Constructor<TService>, name: string): TService;
-    tryResolve<TService, TArg1>(ctor: Constructor<TService>, arg1: TArg1): TService;
-    lazyResolve<TService, TArg1>(ctor: Constructor<TService>, arg1: TArg1): TService;
-    lazyResolveNamed<TService>(ctor: Constructor<TService>, name: string): () => TService;
-    resolveNamed<TService, TArg1>(ctor: Constructor<TService>, name: string, arg1: TArg1): TService;
-    resolve<TService, TArg1, TArg2>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2): TService;
-    tryResolve<TService, TArg1, TArg2>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2): TService;
-    lazyResolveNamed<TService, TArg1>(ctor: Constructor<TService>, name: string, arg1: TArg1): TService;
-    lazyResolve<TService, TArg1, TArg2>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2): TService;
-    resolveNamed<TService, TArg1, TArg2>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2): TService;
-    resolve<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
-    tryResolve<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, arg1: TArg1, arg: TArg2, arg3: TArg3): TService;
-    lazyResolveNamed<TService, TArg1, TArg2>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2): TService;
-    lazyResolve<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
-    resolveNamed<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
-    resolve<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
-    lazyResolveNamed<TService, TArg1, TArg2, TArg3>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3): TService;
-    tryResolve<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
-    lazyResolve<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
-    resolveNamed<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
-    lazyResolveNamed<TService, TArg1, TArg2, TArg3, TArg4>(ctor: Constructor<TService>, name: string, arg1: TArg1, arg2: TArg2, arg3: TArg3, arg4: TArg4): TService;
-}
-
-declare type Func<T1, TResult> = (arg1: T1) => TResult;
-declare type Action<T1, T2> = (arg1: T1, arg2: T2) => void;
-declare type Constructor<TService> = (new (...args: any[]) => TService) | string;
