0e1e82c40d3ae44da35d8b9b9e09efba541a84f2
Sagi <sagifogel@hotmail.com>
2016-04-21 20:51:31 +0300

    version updated

--- dist/TSFunq.js
+++ dist/TSFunq.js
@@ -1,5 +1,5 @@
 /**
- * TSFunq v.1.0.0 - Funq Dependency injection container port for TypeScript
+ * TSFunq v.1.0.1 - Funq Dependency injection container port for TypeScript
  * Copyright (c) 2015 Sagi Fogel
  * MIT License
  * https://github.com/sagifogel/TSFunq
